64b1c4426448f26bd0f2d8fa72e36cd96e213e25
gre123 <godzilla123@tlen.pl>
2016-05-18 23:36:30 +0200

    proba dzialania pulla

--- GitBK/src/gitbk/GitCommandsController.java
+++ GitBK/src/gitbk/GitCommandsController.java
@@ -43,6 +43,7 @@ public class GitCommandsController {
                 protected Object call() throws Exception {
                     commandStatus = GitFacade.pullRepo(parentController.repository);
                     parentController.loadCurrentRepository(new Git(parentController.repository));
+                    new HighlighterFacade(HighlighterFacade.CodeType.JAVA).displayHighlightedCode("", parentController.sourceCodeView);
                     return commandStatus;
                 }
 
@@ -85,6 +86,7 @@ public class GitCommandsController {
                     protected Object call() throws Exception {
                         GitFacade.commitRepo(parentController.repository, result.get());
                         parentController.loadCurrentRepository(new Git(parentController.repository));
+                        new HighlighterFacade(HighlighterFacade.CodeType.JAVA).displayHighlightedCode("", parentController.sourceCodeView);
                         return commandStatus;
                     }
                 };
